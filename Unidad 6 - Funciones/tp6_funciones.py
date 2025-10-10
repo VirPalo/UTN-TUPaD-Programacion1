66d30a028f005c629289b1196d7dfacc02c53d74
Virginia Paloma <virginiapaloma92@gmail.com>
2025-10-09 21:06:57 -0300

    tp6_funciones - ej 1 al 3

---
+++ Unidad 6 - Funciones/tp6_funciones.py
@@ -0,0 +1,45 @@
+# Ejercicio 1 ----------
+
+# Definición de la función
+"""
+    
+def imprimir_hola_mundo():
+    print('Hola Mundo!')
+
+# Programa principal
+
+imprimir_hola_mundo()
+
+# Ejercicio 2 ----------
+
+# Definición de la función
+
+def saludar_usuario(nombre):
+    print(f'Hola {nombre}!')
+
+# Programa principal
+nombre = input('Ingrese su nombre: ')
+saludar_usuario(nombre)
+
+# Ejercicio 3 ----------
+
+# Definición de la función
+
+def informacion_personal(nombre, apellido, edad, residencia):
+    print(f'Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}.')
+
+# Programa principal
+nombre = input('Ingrese su nombre: ')
+apellido = input('Ingrese su apellido: ')
+edad = int(input('Ingrese su edad: '))
+residencia = input('Ingrese su lugar de residencia: ')
+
+informacion_personal(nombre, apellido, edad, residencia)
+
+"""
+
+# Ejercicio 4 ----------
+
+# Definición de la función
+
+# Programa principal
