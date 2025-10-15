6a6071d7c441a453902e9f2652fb8694b8fa2acd
Virginia Paloma <virginiapaloma92@gmail.com>
2025-10-15 12:04:54 -0300

    tp7 - Hasta ej 6 sin terminar

--- Unidad 7 - TDA/tp7_tda.py
+++ Unidad 7 - TDA/tp7_tda.py
@@ -27,10 +27,10 @@ print(precios_frutas)
 lista_frutas = list(precios_frutas.keys())
 
 print(lista_frutas)
-'''
+
 # Ejercicio 4 ----------
 
-contactos = {}
+contactos = dict()
 
 for i in range(1,6):
     # El usuario ingresa los datos
@@ -38,17 +38,60 @@ for i in range(1,6):
     numero = int(input('Ahora, ingrese el número de teléfono: '))
     
     # Valido que la clave sea unica
-    #while nombre in contactos.keys():
-     #   print('El nombre ya existe.')
-      #  nombre = input('Ingrese un nombre nuevamente: ')
+    while nombre in contactos.keys():
+      print('El nombre ya existe. Intente nuevamente')
+      nombre = input(f'Ingrese el nombre del contacto {i}: ')
+      numero = int(input('Ahora, ingrese el número de teléfono: '))
         
     contactos[nombre] = numero
 
 print(contactos)
-    
-    
+
+nombre_pedido = input('Ingrese un nombre para mostrar el número de telefono asociado: ')
+if nombre_pedido in contactos.keys():
+  print(f'El número de {nombre_pedido} es {contactos[nombre_pedido]}')
+else:
+  print('El nombre no existe.')
+   
 # Ejercicio 5 ----------
+
+frase = input('Ingrese una frase: ')
+
+palabras_set = set(frase.split())
+print(palabras_set)
+
+palabras = frase.split()
+recuento = dict()
+
+for palabra in palabras:
+  palabra_unica = palabra.lower()
+  if palabra_unica in recuento:
+    recuento[palabra_unica] += 1
+  else:
+    recuento[palabra_unica] = 1
+  
+print(recuento)
+''' 
 # Ejercicio 6 ----------
+
+alumnos = dict()
+
+for i in range(3):
+  nombre = input(f'Ingrese el nombre del alumno/a {i+1}: ')
+  print(f'Ahora ingrese las notas del alumno/a {nombre}')
+  
+  for j in range(3):
+    notas = list()
+    nota = float(input(f'Ingrese la nota {j+1}: '))
+    notas.append(nota)
+  
+  #notas = tuple(notas)
+  alumnos[nombre] = notas
+
+print(alumnos)
+
+
+
 # Ejercicio 7 ----------
 # Ejercicio 8 ----------
 # Ejercicio 9 ----------
