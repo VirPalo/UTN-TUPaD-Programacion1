6751f7d8eefc7b46d137ee6f01acec762e1f7d40
Virginia Paloma <virginiapaloma92@gmail.com>
2025-10-22 21:00:43 -0300

    Resuelto hasta el ej. 7

--- Unidad 7 - TDA/tp7_tda.py
+++ Unidad 7 - TDA/tp7_tda.py
@@ -71,29 +71,71 @@ for palabra in palabras:
     recuento[palabra_unica] = 1
   
 print(recuento)
-''' 
+
 # Ejercicio 6 ----------
 
 alumnos = dict()
 
 for i in range(3):
+  #El usuario ingresa el nombre del alumno/a
   nombre = input(f'Ingrese el nombre del alumno/a {i+1}: ')
   print(f'Ahora ingrese las notas del alumno/a {nombre}')
   
+  #El usuario ingresa cada nota y la guardo en una lista
+  # En el mismo bucle calculo el promedio
+  notas = list()
+  promedio = 0
   for j in range(3):
-    notas = list()
     nota = float(input(f'Ingrese la nota {j+1}: '))
     notas.append(nota)
-  
-  #notas = tuple(notas)
+    promedio += nota
+    
+  #Muestro el promedio
+  print(f'El promedio de {nombre} es {(promedio / 3):.2f}.')
+    
+  #Convierto la lista a tupla y la guardo en el diccionario
+  notas = tuple(notas)
   alumnos[nombre] = notas
-
-print(alumnos)
-
-
-
+  
 # Ejercicio 7 ----------
+
+p1 = {1000, 1010, 1003, 1011, 1012, 1014, 1025, 1033}
+p2 = {1000, 1001, 1002, 1003, 1005, 1011, 1013, 1033}
+
+# Alumnos que aprobaron ambos parciales
+ambos = list()
+
+ambos = p1.intersection(p2) # Los valores que estan en los dos conjuntos 
+print(f'Los alumnos que aprobaron los dos parciales son: {ambos}.')
+
+# Alumnos que aprobaron solo uno de los parciales
+solo_uno = list()
+
+solo_uno = p1.symmetric_difference(p2) # Dif simetrica: Los valores que estan en uno u otro conjunto pero no en ambos.
+print(f'Los alumnos que aprobaron solo uno de los dos parciales son: {solo_uno}.')
+
+# Alumnos que aprobaron al menos uno de los parciales
+al_menos_uno = list()
+
+al_menos_uno = p1.union(p2) # Todos los valores de ambos conjuntos
+print(f'Los alumnos que aprobaron al menos uno de los dos parciales son: {al_menos_uno}.')
+''' 
 # Ejercicio 8 ----------
+
+#productos = dict()
+
 # Ejercicio 9 ----------
+
+agenda = {
+  ('lunes','10:00'): 'Reunión',
+  ('lunes', '12:00'): 'Almuerzo con cliente',
+  ('martes', '15:00'): 'Clase de Inglés',
+  ('miercoles', '21:00'): 'Cumpleaños Abuela',
+  ('jueves', '12:00'): 'Videollamada',
+  ('viernes', '18:00'): 'Turno peluquería'          
+}
+
+print()
+
 # Ejercicio 10 ----------
 
